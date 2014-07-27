9b0139f25f06aa24027d9e1169bab083b116f2dc
jadedgnome <jadedgnome@gmail.com>
2014-07-27 19:58:25 -0400

    escape .

--- run-first-configure.sh
+++ run-first-configure.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # This Script will amend the simpletum scripts with your working directory for api.key and json.sh.
-# If you want to run simpletum from /home/john/simpletum/ . this script will prepend /home/john/simpletum/ to api.key and json.sh. e.x (/home/john/simpletum/api.key)
+# If you want to run simpletum from /home/john/simpletum/ . This script will prepend /home/john/simpletum/ to api.key and json.sh. e.x (/home/john/simpletum/api.key)
 
 sed "s|api\.key|$PWD/api.key|" -i simplesou.sh ;
 
